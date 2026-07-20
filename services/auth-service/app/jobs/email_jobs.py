4d4499d52ff78c7157fc7a4ed49be449950b66bf
Saransh-Basu-01 <saranshbasu@gmail.com>
2026-07-20 19:16:16 +0545

    added new job

--- services/auth-service/app/jobs/email_jobs.py
+++ services/auth-service/app/jobs/email_jobs.py
@@ -34,3 +34,31 @@ class PasswordResetEmailJob:
             job_id=data["job_id"],
             created_at=data["created_at"]
         )
+    
+
+@dataclass(frozen=True)
+class PasswordChangedEmailJob:
+    job_type: str
+    recipient_email: str
+    job_id: str
+    created_at:str
+    @classmethod
+    def create(cls, recipient_email: str) -> "PasswordChangedEmailJob":
+        return cls(
+            job_type="password_changed_email",
+            recipient_email=recipient_email,
+            job_id=str(uuid4()),
+            created_at=datetime.now(timezone.UTC).isoformat()
+        )
+
+    def to_dict(self) -> dict[str, str]:
+        return asdict(self)
+    
+    @classmethod
+    def from_dict(cls, data: dict[str, str]) -> "PasswordChangedEmailJob":
+        return cls(
+            job_type=data["job_type"],
+            recipient_email=data["recipient_email"],
+            job_id=data["job_id"],
+            created_at=data["created_at"]
+        )
