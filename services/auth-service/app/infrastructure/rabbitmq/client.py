8ac3403a6d96d9189669ad375fcd519dd2beffe4
Saransh-Basu-01 <saranshbasu@gmail.com>
2026-08-03 18:16:50 +0545

    added get rabbitmq connection

--- services/auth-service/app/infrastructure/rabbitmq/client.py
+++ services/auth-service/app/infrastructure/rabbitmq/client.py
@@ -3,3 +3,14 @@ import aio_pika
 from typing import Optional
 from aio_pika.abc import AbstractRobustConnection
 
+from app.core.config import settings
+
+_rabbitmq_connection: Optional[AbstractRobustConnection] = None
+
+async def get_rabbitmq_connenction()->AbstractRobustConnection:
+    global _rabbitmq_connection
+    if _rabbitmq_connection is None or _rabbitmq_connection.is_closed:
+        _rabbitmq_connection=await aio_pika.connect_robust(
+            settings.RABBITMQ_URL
+        )
+    return _rabbitmq_connection
