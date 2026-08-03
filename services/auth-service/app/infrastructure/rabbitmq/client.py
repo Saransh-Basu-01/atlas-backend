247e28b9e456568b3d0e67c52f3015fdfd497219
Saransh-Basu-01 <saranshbasu@gmail.com>
2026-08-03 18:22:29 +0545

    push

--- services/auth-service/app/infrastructure/rabbitmq/client.py
+++ services/auth-service/app/infrastructure/rabbitmq/client.py
@@ -1,24 +1,37 @@
 from __future__ import annotations
-import aio_pika
+
 from typing import Optional
+
+import aio_pika
 from aio_pika.abc import AbstractRobustConnection
 
 from app.core.config import settings
 
-_rabbitmq_connection: Optional[AbstractRobustConnection] = None
 
-async def get_rabbitmq_connenction()->AbstractRobustConnection:
+_rabbitmq_connection: Optional[AbstractRobustConnection] = None
+#This prevents creating a new RabbitMQ connection every time another part of the application needs RabbitMQ.
+
+async def get_rabbitmq_connection() -> AbstractRobustConnection:
     global _rabbitmq_connection
-    if _rabbitmq_connection is None or _rabbitmq_connection.is_closed:
-        _rabbitmq_connection=await aio_pika.connect_robust(
+
+    if (
+        _rabbitmq_connection is None
+        or _rabbitmq_connection.is_closed
+    ):
+        _rabbitmq_connection = await aio_pika.connect_robust(
             settings.RABBITMQ_URL
         )
+
     return _rabbitmq_connection
+
 
 async def close_rabbitmq_connection() -> None:
     global _rabbitmq_connection
 
-    if _rabbitmq_connection is not None and not _rabbitmq_connection.is_closed:
+    if (
+        _rabbitmq_connection is not None
+        and not _rabbitmq_connection.is_closed
+    ):
         await _rabbitmq_connection.close()
 
     _rabbitmq_connection = None
