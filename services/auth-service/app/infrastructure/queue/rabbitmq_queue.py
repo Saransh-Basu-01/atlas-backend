045415967b3c33e06c1cb397b3d13c596376ec7a
Saransh-Basu-01 <saranshbasu@gmail.com>
2026-08-03 18:40:20 +0545

    added enqueue operation

--- services/auth-service/app/infrastructure/queue/rabbitmq_queue.py
+++ services/auth-service/app/infrastructure/queue/rabbitmq_queue.py
@@ -5,3 +5,12 @@ from typing import Any
 
 from app.infrastructure.queue.base import QueueClient
 from app.infrastructure.rabbitmq.client import get_rabbitmq_connection,close_rabbitmq_connection
+
+class RabbitmqQueueClient(QueueClient):
+    def __init__(self)->None:
+        self._client=get_rabbitmq_connection()
+
+    async def enqueue(self, queue_name:str, payload:dict[str,Any]):
+        return await super().enqueue(queue_name, json.dumps(payload))
+
+    
