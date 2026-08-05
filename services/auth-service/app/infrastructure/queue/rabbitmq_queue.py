38e2a515d199eee990fd7c9fbe28fa449b3b5c55
Saransh-Basu-01 <saranshbasu@gmail.com>
2026-08-05 17:25:32 +0545

    refactor enqueue with abs

--- services/auth-service/app/infrastructure/queue/rabbitmq_queue.py
+++ services/auth-service/app/infrastructure/queue/rabbitmq_queue.py
@@ -11,9 +11,12 @@ from app.infrastructure.rabbitmq.client import get_rabbitmq_connection,close_rab
 EMAIL_EXCHANGE_NAME = "email.exchange"
 EMAIL_ROUTING_KEY = "email.send"
 class RabbitMQQueueClient(QueueClient):
-    def __init__(self)->None:
+    def __init__(self,exchange_name: str,
+        routing_key: str,)->None:
         self._connection:AbstractRobustConnection|None=None
         self._channel:AbstractRobustChannel|None=None
+        self._exchange_name = exchange_name
+        self._routing_key = routing_key
 
     async def _ensure_channel(self)->AbstractRobustChannel:
         if self._connection is None or self._connection.is_closed:
@@ -22,19 +25,26 @@ class RabbitMQQueueClient(QueueClient):
             self._channel=await self._connection.channel()
         return self._channel  
 
+    async def _ensure_topology(self, queue_name: str) -> tuple[aio_pika.abc.AbstractRobustExchange, aio_pika.abc.AbstractRobustQueue]:
+        channel = await self._ensure_channel()
+
+        exchange = await channel.declare_exchange(
+            self._exchange_name,
+            type=ExchangeType.DIRECT,
+            durable=True,
+        )
+
+        queue = await channel.declare_queue(
+            queue_name,
+            durable=True,
+        )
+
+        await queue.bind(exchange, routing_key=self._routing_key)
+        return exchange, queue
+
 
     async def enqueue(self, queue_name: str, payload: dict[str, Any]) -> None:
-        channel = await self._ensure_channel()
-        exchange=await channel.declare_exchange(
-            EMAIL_EXCHANGE_NAME,
-            type=ExchangeType.DIRECT,
-            durable=True
-        )
-        queue=await channel.declare_queue(
-            queue_name,
-            durable=True
-        )
-        await queue.bind(exchange,routing_key=EMAIL_ROUTING_KEY)
+        exchange, _ =await self._ensure_topology(queue_name)
         message = aio_pika.Message(
             body=json.dumps(payload).encode("utf-8"),
             content_type="application/json",
