c00c4a1352f7ac19cbdb34c86b8bd83fa5d10aee
Saransh-Basu-01 <saranshbasu@gmail.com>
2026-08-11 20:06:30 +0545

    adding instacne

--- services/auth-service/app/models/__init__.py
+++ services/auth-service/app/models/__init__.py
@@ -1,4 +1,5 @@
 from app.models.models import User
 from app.models.refresh_token import RefreshToken
 from app.models.password_reset_token import PasswordResetToken
-__all__ = ["User","RefreshToken","PasswordResetToken"]
+from app.models.oauth_accounts import OAuthAccount
+__all__ = ["User","RefreshToken","PasswordResetToken","OAuthAccount"]
