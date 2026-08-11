7d5ab76fb0d836e39a8feb4daf0589c928bcadf1
Saransh-Basu-01 <saranshbasu@gmail.com>
2026-08-11 18:45:15 +0545

    adding imports

--- services/auth-service/app/api/v1/auth.py
+++ services/auth-service/app/api/v1/auth.py
@@ -13,6 +13,7 @@ from app.repositories.password_reset_token_repository import PasswordResetTokenR
 from app.services.email_service import EmailService
 from app.services.password_reset_service import PasswordResetService,InvalidOrExpiredResetTokenError
 from app.infrastructure.queue.redis_queue import RedisQueueClient
+from app.infrastructure.queue.rabbitmq_queue import RabbitMQQueueClient
 
 
 
