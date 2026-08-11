d6ab05cbf1e21aa2bb4c463d1d4a20bd39cc7685
Saransh-Basu-01 <saranshbasu@gmail.com>
2026-08-11 19:57:41 +0545

    re coding models for oauth

--- services/auth-service/app/models/models.py
+++ services/auth-service/app/models/models.py
@@ -19,7 +19,7 @@ class User(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column(String(50), nullable=False, unique=True,index=True)
     email: Mapped[str] = mapped_column(String(120), nullable=False, unique=True,index=True)
-    password_hash: Mapped[str] = mapped_column(String(255), nullable=False)
+    password_hash: Mapped[str] = mapped_column(String(255), nullable=True)
 
     created_at: Mapped[datetime] = mapped_column(
         server_default=func.now(),
