186b1ea442c4439f2bffdc6f17818b24d8c61449
Saransh-Basu-01 <saranshbasu@gmail.com>
2026-08-11 18:44:17 +0545

    changed

--- services/auth-service/app/api/v1/auth.py
+++ services/auth-service/app/api/v1/auth.py
@@ -12,9 +12,8 @@ from app.core.config import settings
 from app.repositories.password_reset_token_repository import PasswordResetTokenRepository
 from app.services.email_service import EmailService
 from app.services.password_reset_service import PasswordResetService,InvalidOrExpiredResetTokenError
-from fastapi import BackgroundTasks
 from app.infrastructure.queue.redis_queue import RedisQueueClient
-from app.infrastructure.redis import client
+
 
 
 
