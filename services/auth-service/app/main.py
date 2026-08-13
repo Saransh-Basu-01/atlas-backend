c327135bb39d3c31cf843c65c4252b9f4eb29c95
Saransh-Basu-01 <saranshbasu@gmail.com>
2026-08-13 17:31:56 +0545

    done with redirecting in google

--- services/auth-service/app/main.py
+++ services/auth-service/app/main.py
@@ -5,6 +5,8 @@ from app.api.health import router as health_router
 from app.api.v1.auth import router as auth_router
 from app.api.v1.oauth_google import router as oauth_router
 
+from fastapi.middleware.cors import CORSMiddleware
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # Startup
@@ -29,6 +31,18 @@ def root():
     return {
         "message": "Auth Service Running"
     }
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(health_router)
 app.include_router(auth_router)
