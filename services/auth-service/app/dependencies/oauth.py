1c48567665129690dba0316d2914fe11c2f28033
Saransh-Basu-01 <saranshbasu@gmail.com>
2026-08-19 20:39:32 +0545

    added session new dependencies

--- services/auth-service/app/dependencies/oauth.py
+++ services/auth-service/app/dependencies/oauth.py
@@ -1,11 +1,19 @@
 from __future__ import annotations
-
+from fastapi import Depends
 from app.core.config import settings
 from app.infrastructure.oauth.google.client import GoogleOAuthClient
 from app.services.oauth_service import GoogleOAuthService
+from app.core.config import settings
+from app.repositories.user_repository import UserRepository
+from repositories.oauth_repository import OAuthAccountRepository
+from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.db.session import get_db
 
-def get_google_oauth_service() -> GoogleOAuthService:
+def get_google_oauth_service(
+         session: AsyncSession = Depends(get_db),
+) -> GoogleOAuthService:
     google_client = GoogleOAuthClient(
         client_id=settings.GOOGLE_CLIENT_ID,
         client_secret=settings.GOOGLE_CLIENT_SECRET,
