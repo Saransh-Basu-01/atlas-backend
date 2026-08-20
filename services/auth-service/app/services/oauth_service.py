1938992c25e4ab4b183a2e9b1817bcacb3abf874
Saransh-Basu-01 <saranshbasu@gmail.com>
2026-08-20 21:20:22 +0545

    now done committing to db too

--- services/auth-service/app/services/oauth_service.py
+++ services/auth-service/app/services/oauth_service.py
@@ -99,6 +99,6 @@ class GoogleOAuthService:
         await self._oauth_account_repository.create(
             oauth_account
         )
-        await self.session.commit()
+        await self._user_repository.commit()
 
         return user
